18c0ab8b38624e03ed06aa001ee98fd93839190b
David Harvey <dharvey@spartaglobal.com>
2020-07-10 12:07:35 +0100

    Explored variables and data types

---
+++ Introduction/hello_data_types.py
@@ -0,0 +1,18 @@
+a = "Aardvark"
+b = "Bat"
+c = "Cheshire Cat"
+d = 5
+
+print(a + ", " + b + ", " + c + " " + str(d))
+
+
+print(f"{a}, {b}, {c} {d}")
+
+
+name = "David"
+print("Hello my name is " + name + "!")
+
+print(f"Hello, my name is {name}!")
+
+
+# Name, age, number of siblings, favourite decimal number, favourite animal
---
+++ Introduction/hello_variables.py
@@ -0,0 +1,27 @@
+# a = 5
+# print(a)
+# b = 2
+# c = "Hello!"
+# d = 0.25
+# e = True
+# a = (2, 3, 4)
+#
+# print(a)
+# print(b)
+# print(c)
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+print("What is your name?")
+
+name = input()
+print("Hello...")
+print(name)
+
+# Use input to collect name, age and DOB from user
+# Print them to the console with a greeting
+# What type is captured?
